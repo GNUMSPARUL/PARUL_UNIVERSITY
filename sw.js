a2a9e344834e8cb2df035efb885af3a89f599f2d
GNUMS <vardhan.yahoo999@gmail.com>
2026-04-14 04:36:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-// This is the minimum code required for PWA installability
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  console.log('SW Installed');
 });
-
-self.addEventListener('fetch', (event) => {
-  // Even an empty fetch handler allows the PWA to be installed
+self.addEventListener('fetch', (e) => {
+  // Required for PWA
 });
